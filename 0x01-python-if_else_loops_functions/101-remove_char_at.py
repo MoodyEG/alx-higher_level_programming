64b2c4e7e36c3b6b4e75d4442c32d6bc0f5d32b5
MoodyEG <moodyeg2011@yahoo.com>
2024-01-02 17:10:23 -0800

    done fast

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	cpy = ""
-	for i in range(len(str)):
-		if i != n:
-			cpy += str[i]
-	return cpy
+    cpy = ""
+    for i in range(len(str)):
+        if i != n:
+            cpy += str[i]
+    return cpy
