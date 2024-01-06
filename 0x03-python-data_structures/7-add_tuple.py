53235f53a7ff90ee4270dfa9490a5f1d252ccbf9
MoodyEG <moodyeg2011@yahoo.com>
2024-01-05 21:16:12 -0800

    done fast

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tlist =[[0, 0], [0, 0]]
+    tlist = [[0, 0], [0, 0]]
     if len(tuple_a) > 0:
         tlist[0][0] = tuple_a[0]
         if len(tuple_a) > 1:
-             tlist[0][1] = tuple_a[1]
+            tlist[0][1] = tuple_a[1]
     if len(tuple_b) > 0:
         tlist[1][0] = tuple_b[0]
         if len(tuple_b) > 1:
-             tlist[1][1] = tuple_b[1]
+            tlist[1][1] = tuple_b[1]
     new_tuple = (tlist[0][0] + tlist[1][0]), (tlist[0][1] + tlist[1][1])
     return new_tuple
