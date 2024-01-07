5e20b43c2a6c90c7d103275075e6f4843970432d
MoodyEG <moodyeg2011@yahoo.com>
2024-01-07 06:44:52 -0800

    Going insane

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
+struct timespec;
+
 #include <Python.h>
-#include "listobject.h"
-#include "object.h"
 
 /**
  * print_python_list_info - prints some basic info about Python lists.
