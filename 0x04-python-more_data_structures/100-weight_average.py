b71bf580623d5182b29858ac8b1891559156f4d3
MoodyEG <moodyeg2011@yahoo.com>
2024-01-10 12:09:53 -0800

    done fast

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    top, bot = 0, 0
-    for i, j in my_list:
-        top += i * j
-        bot += i
-    return top / bot
+
