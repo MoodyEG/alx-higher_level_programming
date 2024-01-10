53b3b30b26e5099e28809f704dc951a20d006665
MoodyEG <moodyeg2011@yahoo.com>
2024-01-10 11:51:33 -0800

    done fast

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
-    if roman_string is (not str):
+    if not isinstance(roman_string, str):
         return 0
     codex = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     total = 0
