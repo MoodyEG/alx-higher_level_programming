a2ec57e790b302063cef5107bb04b35464b00dc4
MoodyEG <moodyeg2011@yahoo.com>
2024-01-10 12:25:05 -0800

    done fast

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    return list(map(lambda x: x**2,my_list))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
