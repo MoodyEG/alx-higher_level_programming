98b8e88371feb31e6fb3af47b16d6f077dc62ddc
MoodyEG <moodyeg2011@yahoo.com>
2024-01-10 12:19:44 -0800

    done fast

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    return list(map(lambda x: x**2,my_list))
---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,5 @@
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary):
+        if a_dictionary[i] == value:
+            del a_dictionary[i]
+    return a_dictionary
