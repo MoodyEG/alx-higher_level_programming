534a09d353ae497e014a44c07ff1600ea4c0e5f8
MoodyEG <moodyeg2011@yahoo.com>
2024-02-01 06:56:08 -0800

    done fast

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,3 +17,19 @@ class Square:
     def area(self):
         """Return the area of the square."""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        """Set a new value for the size attribute."""
+        if isinstance(new_size, int):
+            if new_size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = new_size
+        else:
+            raise TypeError("size must be an integer")
