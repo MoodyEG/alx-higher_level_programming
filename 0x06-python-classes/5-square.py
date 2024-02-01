2603b85fae7e443d82e2ded76495494304056294
MoodyEG <moodyeg2011@yahoo.com>
2024-02-01 07:00:39 -0800

    done fast

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Main module."""
+
+
+class Square:
+    """Class Square that defines a square."""
+    def __init__(self, size=0):
+        """Initialize the Square class."""
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        """Set a new value for the size attribute."""
+        if isinstance(new_size, int):
+            if new_size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = new_size
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """Print out the square in #."""
+        if self.__size is 0:
+            print()
+        else:
+            for j in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
