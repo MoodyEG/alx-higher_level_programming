b684d405bc4b5ac7a50fbd0bc1e4abfe2d62b8eb
MoodyEG <moodyeg2011@yahoo.com>
2024-02-12 10:30:50 -0800

    testing task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base module."""
+import json
 
 
 class Base:
@@ -13,3 +14,53 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Convert list of dictionaries into a JSON string """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save the content of the list in a file """
+        filename = "{}.json".format(cls.__name__)
+        if list_objs is None:
+            pass
+        else:
+            attrs = []
+            for i in range(len(list_objs)):
+                attrs.append(list_objs[i].to_dictionary())
+            jattrs = cls.to_json_string(attrs)
+            with open(filename, 'w') as file:
+                file.write(jattrs)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Load data from a JSON string """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create an instance based on a dictionary """
+        dummy = cls(10, 10)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Load objects from a file and returns them in a list"""
+        filename = "{}.json".format(cls.__name__)
+        datalist = []
+        try:
+            with open(filename,'r') as file:
+                filelist = cls.from_json_string(file.read())
+            for obj in range(len(filelist)):
+                datalist.append(cls.create(**filelist[obj]))
+        except:
+            pass
+        return datalist
+
