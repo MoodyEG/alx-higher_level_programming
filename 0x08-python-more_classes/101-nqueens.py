474fdbfce5581ba139c007a2dbcaa63ef3fc45fa
MoodyEG <moodyeg2011@yahoo.com>
2024-02-15 00:29:18 -0800

    done fast

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -42,13 +42,12 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
-        print("Usage: nqueens N\n")
+        print("Usage: nqueens N")
         sys.exit(1)
-    try:
-        size = int(sys.argv[1])
-    except TypeError:
+    if not isinstance(sys.argv[1], int):
         print("N must be a number")
         sys.exit(1)
+    size = int(sys.argv[1])
     if size < 4:
         print("N must be at least 4")
         sys.exit(1)
