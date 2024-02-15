ad8a9410bce897dcc08b5c7ad8a56ae3fe71ca46
MoodyEG <moodyeg2011@yahoo.com>
2024-02-15 01:33:39 -0800

    done fast

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Main Module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ A function that inserts a line of text to a file,
+    after each line containing a specific string """
+    with  open(filename) as file:
+        lines = file.readlines()
+    modified_lines = []
+    for line in lines:
+        if search_string in line:
+            modified_lines.append(line + "\n" + new_string + "\n")
+        else:
+            modified_lines.append(line)
+    with open(filename, "w") as file:
+        file.writelines(modified_lines)
