85216858e2fcdb26822c07d365aba4ef9bd35397
MoodyEG <moodyeg2011@yahoo.com>
2024-02-15 01:10:14 -0800

    done fast

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(really=[]):
+    really += ["BestSchool"]
+    return ", ".join(really)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Main Module """
+
+
+class LockedClass:
+    __slots__ = ['first_name']
+    pass
