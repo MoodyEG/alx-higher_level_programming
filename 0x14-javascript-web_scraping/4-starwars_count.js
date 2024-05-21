24202514eec7eea02b577be97743f97c5265e94c
MoodyEG <moodyeg2011@yahoo.com>
2024-05-21 17:29:41 +0300

    done fast

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const urlId = 'https://swapi-api.alx-tools.com/api/people/18/';
-const film = process.argv[2];
-request.get(film, (err, res, body) => {
+/* const urlId = 'https://swapi-api.alx-tools.com/api/people/18/'; */
+request.get(process.argv[2], (err, res, body) => {
   if (err) {
     console.log(err);
   }
-  const data = JSON.parse(body);
-  const movies = data.results.filter(
-    movie => movie.characters.includes(urlId));
-  console.log(movies.length);
+  let count = 0;
+  const data = JSON.parse(body).results;
+  for (const films in data) {
+    const chars = data[films].characters;
+    for (const char in chars) {
+      if (chars[char].includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
